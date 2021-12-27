2e5c3d39fe8b4ab877ccfe7c1690a0c78157927b
Guillaume Quintard <guillaume.quintard@gmail.com>
2021-12-27 09:56:06 -0800

    use set -e to automate checks

--- script/test/test.sh
+++ script/test/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 VRD=$(cd $(dirname $0)/../../; pwd)
 cd ${VRD}
 
@@ -24,66 +26,44 @@ export VMP_DBG_CACHE=1
 
 #ubuntu
 ./vmod-packager.sh -t -d focal -v 6.0.8 test-varnish-modules60 && ls ${VRD}/pkgs/debs/test-varnish-modules60/test-varnish-modules60_71.0.1~focal-1_amd64.deb
-if [ $? -ne 0 ]; then exit 1; fi
 ./vmod-packager.sh -t -d focal -v 7.0.0 test-varnish-modules70 && ls ${VRD}/pkgs/debs/test-varnish-modules70/test-varnish-modules70_140.0.1~focal-1_amd64.deb
-if [ $? -ne 0 ]; then exit 1; fi
 
 ./vmod-packager.sh -t -d bionic -v 6.0.8 test-varnish-modules60 && ls ${VRD}/pkgs/debs/test-varnish-modules60/test-varnish-modules60_71.0.1~bionic-1_amd64.deb
-if [ $? -ne 0 ]; then exit 1; fi
 ./vmod-packager.sh -t -d bionic -v 7.0.0 test-varnish-modules70 && ls ${VRD}/pkgs/debs/test-varnish-modules70/test-varnish-modules70_140.0.1~bionic-1_amd64.deb
-if [ $? -ne 0 ]; then exit 1; fi
 
 #debian
 ./vmod-packager.sh -t -d buster -v 6.0.8 test-varnish-modules60 && ls ${VRD}/pkgs/debs/test-varnish-modules60/test-varnish-modules60_71.0.1~buster-1_amd64.deb
-if [ $? -ne 0 ]; then exit 1; fi
 ./vmod-packager.sh -t -d buster -v 7.0.0 test-varnish-modules70 && ls ${VRD}/pkgs/debs/test-varnish-modules70/test-varnish-modules70_140.0.1~buster-1_amd64.deb
-if [ $? -ne 0 ]; then exit 1; fi
 
 ./vmod-packager.sh -t -d bullseye -v 6.0.8 test-varnish-modules60 && ls ${VRD}/pkgs/debs/test-varnish-modules60/test-varnish-modules60_71.0.1~bullseye-1_amd64.deb
-if [ $? -ne 0 ]; then exit 1; fi
 ./vmod-packager.sh -t -d bullseye -v 7.0.0 test-varnish-modules70 && ls ${VRD}/pkgs/debs/test-varnish-modules70/test-varnish-modules70_140.0.1~bullseye-1_amd64.deb
-if [ $? -ne 0 ]; then exit 1; fi
 
 #centos
 ./vmod-packager.sh -t -d centos8 -v 6.0.8 test-varnish-modules60 && ls ${VRD}/pkgs/rpms/test-varnish-modules60/test-varnish-modules60-71.0.1-1.el8.x86_64.rpm
-if [ $? -ne 0 ]; then exit 1; fi
 ./vmod-packager.sh -t -d centos8 -v 7.0.0 test-varnish-modules70 && ls ${VRD}/pkgs/rpms/test-varnish-modules70/test-varnish-modules70-140.0.1-1.el8.x86_64.rpm
-if [ $? -ne 0 ]; then exit 1; fi
 
 #https://github.com/varnishcache/varnish-cache/commit/454733b82a3279a1603516b4f0a07f8bad4bcd55
 ./vmod-packager.sh -t -d focal -c 454733b82a3279a1603516b4f0a07f8bad4bcd55 -p trunk- test-varnish-modules70 && ls ${VRD}/pkgs/debs/test-varnish-modules70/trunk-test-varnish-modules70_140.0.1~focal-1_amd64.deb
-if [ $? -ne 0 ]; then exit 1; fi
 ./vmod-packager.sh -t -d centos8 -c 454733b82a3279a1603516b4f0a07f8bad4bcd55 -p trunk- test-varnish-modules70 && ls ${VRD}/pkgs/rpms/test-varnish-modules70/trunk-test-varnish-modules70-140.0.1-1.el8.x86_64.rpm 
-if [ $? -ne 0 ]; then exit 1; fi
 
 
 ./vmod-packager.sh -t -d focal -v 7.0.0 src/test-libvmod-digest70
-if [ $? -eq 0 ]; then exit 1; fi
 ./vmod-packager.sh -t -d centos8 -v 7.0.0 src/test-libvmod-digest70
-if [ $? -eq 0 ]; then exit 1; fi
 
 cp ${VRD}/sample-src/libvmod-digest_init.sh ${VRD}/src/test-libvmod-digest70_init.sh 
 cp ${VRD}/sample-src/libvmod-digest_env.sh  ${VRD}/src/test-libvmod-digest70_env.sh 
 
 ./vmod-packager.sh -t -d focal -v 7.0.0 src/test-libvmod-digest70 && ls ${VRD}/pkgs/debs/test-libvmod-digest70/test-libvmod-digest70_140.0.1~focal-1_amd64.deb
-if [ $? -ne 0 ]; then exit 1; fi
 ./vmod-packager.sh -t -d centos8 -v 7.0.0 src/test-libvmod-digest70 && ls ${VRD}/pkgs/rpms/test-libvmod-digest70/test-libvmod-digest70-140.0.1-1.el8.x86_64.rpm
-if [ $? -ne 0 ]; then exit 1; fi
 
 cp ${VRD}/sample-src/libvdp-pesi_config.sh ${VRD}/src/test-libvdp-pesi70_config.sh
 cp ${VRD}/sample-src/libvdp-pesi_init.sh  ${VRD}/src/test-libvdp-pesi70_init.sh
 
 ./vmod-packager.sh -t -d focal -k -v 7.0.0 src/test-libvdp-pesi70 && ls ${VRD}/pkgs/debs/test-libvdp-pesi70/test-libvdp-pesi70_140.0.1~focal-1_amd64.deb && ls ${VRD}/pkgs/debs/varnish/varnish_7.0.0-1vmp~focal_amd64.deb
-if [ $? -ne 0 ]; then exit 1; fi
 ./vmod-packager.sh -t -d buster -k -v 7.0.0 src/test-libvdp-pesi70 && ls ${VRD}/pkgs/debs/test-libvdp-pesi70/test-libvdp-pesi70_140.0.1~buster-1_amd64.deb && ls ${VRD}/pkgs/debs/varnish/varnish_7.0.0-1vmp~buster_amd64.deb
-if [ $? -ne 0 ]; then exit 1; fi
 ./vmod-packager.sh -t -d centos8 -k -v 7.0.0 src/test-libvdp-pesi70 && ls ${VRD}/pkgs/rpms/test-libvdp-pesi70/test-libvdp-pesi70-140.0.1-1.el8.x86_64.rpm && ls ${VRD}/pkgs/rpms/varnish/varnish-7.0.0-1vmp.el8.x86_64.rpm
-if [ $? -ne 0 ]; then exit 1; fi
 
 ./vmod-packager.sh -t -d focal -k -r varnish-cache src/test-varnish-modules70 && ls ${VRD}/pkgs/debs/test-libvdp-pesi70/test-libvdp-pesi70_140.0.1~focal-1_amd64.deb && ls ${VRD}/pkgs/debs/varnish/varnish_`date +%Y%m%d`.*-1vmp+fromsrc~focal_amd64.deb
-if [ $? -ne 0 ]; then exit 1; fi
 ./vmod-packager.sh -t -d centos8 -k -r varnish-cache src/test-varnish-modules70 && ls ${VRD}/pkgs/rpms/test-libvdp-pesi70/test-libvdp-pesi70-140.0.1-1.el8.x86_64.rpm && ls ${VRD}/pkgs/rpms/varnish/varnish-`date +%Y%m%d`.*-1vmp+fromsrc.el8.x86_64.rpm
-if [ $? -ne 0 ]; then exit 1; fi
-
 
 echo "pass basic test"

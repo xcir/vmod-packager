6081fcc44f764f5c8ec0314eb7e6cd52b19d6486
Shohei Tanaka(@xcir) <kokoniimasu+git@gmail.com>
2022-01-14 12:13:07 +0000

    fix skip test support

--- script/arch/arch-postfilter.sh
+++ script/arch/arch-postfilter.sh
@@ -3,11 +3,17 @@ set -e
 
 echo "VMP>>>$0 : ${VMP_VMOD_NAME}"
 
+if [ ${VMP_SKIP_TEST} -eq 1 ]; then
+    TMP_TEST="--nocheck"
+else
+    TMP_TEST=""
+fi
+
 cd ${VMP_WORK_DIR}
 set -x
 chown builder -R .
 su builder -c "makepkg --force --geninteg" >> PKGBUILD
-su builder -c "makepkg --force --noconfirm --nodeps "
+su builder -c "makepkg --force --noconfirm --nodeps ${TMP_TEST}"
 
 mkdir -p ${VMP_ROOT_DIR}/pkgs/arch/${VMP_VMOD_NAME}
 find ${VMP_WORK_DIR} -type f -name *.tar.zst | xargs -i cp -p {} ${VMP_ROOT_DIR}/pkgs/arch/${VMP_VMOD_NAME}/

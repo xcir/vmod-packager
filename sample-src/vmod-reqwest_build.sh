4ff3f3d48b60adeadc71e6ecadb7ecb2379eae21
Shohei Tanaka(@xcir) <kokoniimasu+git@gmail.com>
2022-01-23 15:12:50 +0000

    wip: custom build(deb)

--- sample-src/vmod-reqwest_build.sh
+++ sample-src/vmod-reqwest_build.sh
@@ -1,11 +1,14 @@
-#!/bin/sh
-
-# https://github.com/gquintard/vmod_reqwest
+#!/bin/bash
 set -e
 
+# https://github.com/gquintard/vmod_reqwest
+
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+source ~/.cargo/env
 
 apt-get install libssl-dev
 cd ${VMP_WORK_DIR}/src
 cargo build --release
 cp ${VMP_WORK_DIR}/src/target/release/libvmod_reqwest.so ${VMP_WORK_DIR}/vmp_build/usr/lib/varnish/vmods/
+

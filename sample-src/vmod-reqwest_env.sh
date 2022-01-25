c87285d94184952da607bbc4fa42546ef3ec2b87
Shohei Tanaka(@xcir) <kokoniimasu+git@gmail.com>
2022-01-25 13:28:30 +0000

    fix

--- sample-src/vmod-reqwest_env.sh
+++ sample-src/vmod-reqwest_env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # https://github.com/gquintard/vmod_reqwest
-export VMP_REQUIRE_DEB=libssl
-export VMP_REQUIRE_RPM=openssl-libs
+#export VMP_REQUIRE_DEB=
+#export VMP_REQUIRE_RPM=
 #export VMP_REQUIRE_ARCH=

8bdfe6660a993d92a9f8b71d985597c4bfb0267c
Shohei Tanaka(@xcir) <kokoniimasu+git@gmail.com>
2022-03-23 04:48:25 +0000

    fix

--- script/rpm/rpm-prefilter.sh
+++ script/rpm/rpm-prefilter.sh
@@ -9,7 +9,7 @@ mkdir -p ${VMP_WORK_DIR}/SOURCES
 cp -rp ${VMP_VMOD_ORG_SRC_DIR}/${VMP_VMOD_NAME} ${VMP_WORK_DIR}/src
 if [ -e ${VMP_VMOD_ORG_SRC_DIR}/${VMP_VMOD_NAME}_config.sh ]; then
     cp ${VMP_VMOD_ORG_SRC_DIR}/${VMP_VMOD_NAME}_config.sh ${VMP_WORK_DIR}/src/__vmod-package_config.sh
-elif [-e ${VMP_VMOD_ORG_SRC_DIR}/${VMP_VMOD_NAME}/vmp_config/${VMP_VMOD_NAME}_config.sh ]; then
+elif [ -e ${VMP_VMOD_ORG_SRC_DIR}/${VMP_VMOD_NAME}/vmp_config/${VMP_VMOD_NAME}_config.sh ]; then
     cp ${VMP_VMOD_ORG_SRC_DIR}/${VMP_VMOD_NAME}/vmp_config/${VMP_VMOD_NAME}_config.sh ${VMP_WORK_DIR}/src/__vmod-package_config.sh
 else
     cp ${VMP_ROOT_DIR}/script/default/default_config.sh  ${VMP_WORK_DIR}/src/__vmod-package_config.sh

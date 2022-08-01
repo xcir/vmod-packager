8ca0b1323caca5eb857742124138edf4fcd9018c
Shohei Tanaka(@xcir) <kokoniimasu+git@gmail.com>
2022-08-01 07:28:53 +0000

    change base version of test

--- config/sample-docker_extrun_env.sh
+++ config/sample-docker_extrun_env.sh
@@ -17,6 +17,6 @@ RUST_ARCH="
     /bin/sh /tmp/rust.sh -y && rm /tmp/rust.sh
     "
 
-export VMP_DOCKER_EXTRUN_focal="${RUST_DEB}"
+export VMP_DOCKER_EXTRUN_jammy="${RUST_DEB}"
 export VMP_DOCKER_EXTRUN_centos8="${RUST_RPM}"
 export VMP_DOCKER_EXTRUN_arch="${RUST_ARCH}"
